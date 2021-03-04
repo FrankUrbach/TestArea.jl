d8a5a566434e921e325f082b3c2e4ea134ba1c54
Frank Urbach <frank.urbach@gmx.de>
2021-03-04 15:51:38 +0100

    small changes

--- mandelbrot.jl
+++ mandelbrot.jl
@@ -1,6 +1,13 @@
 using Memoization
+using BenchmarkTools
 
-@memoize function solvePixel(i; i_n=512, side=1024, center=0.4 + 0.4im, scale=0.3)
+
+const i_n = 512
+const side = 2048
+const center = 0.4+0.4im
+const scale = 0.3
+
+function solvePixel(i; i_n=512, side=1024, center=0.4 + 0.4im, scale=0.3)
     y, x = divrem(i, side)
     c = (x / side * 2 * scale - scale) + (y / side * 2 * scale - scale) * 1im + center
     z = 0im
@@ -9,10 +16,10 @@ using Memoization
         z = z * z + c
         abs(z) <= 2 ? k += 1 : break 
     end
-    return 1 - k / i_n
+    return floor((1 - k / i_n) * 255)
 end
 
-function main(side = 1024)
+function main(side = 2048)
     open("out.ppm", "w") do io
         write(io, "P6 $side $side 255 ")
         buffer = zeros(UInt8, side, side * 3)
@@ -20,14 +27,14 @@ function main(side = 1024)
             tmp = i * side
             for j in 1:side
                 idx = tmp + j
-                res = UInt8(floor(solvePixel(idx) * 255))
+                res = solvePixel(idx; side = side)
                 @inbounds buffer[i+1,j * 3 - 2] = res
                 @inbounds buffer[i+1,j * 3 - 1] = res
                 @inbounds buffer[i+1,j * 3 - 0] = res
             end
         end                     
-        write(io, buffer)       
+        write(io, buffer)   
     end
 end
 
-@time main()
+main()
